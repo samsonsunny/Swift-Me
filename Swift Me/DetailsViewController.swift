c35122984cb5263711740d4d02f2deeed56ccd06
samson <sam.sunny@a-cti.com>
2018-06-14 14:20:45 +0530

    Added json file that will have the information of chapters

--- Swift Me/DetailsViewController.swift
+++ Swift Me/DetailsViewController.swift
@@ -12,17 +12,31 @@ class DetailsViewController: UIViewController {
 
 	public var topic: String = ""
 	
+	@IBOutlet weak var subtitle: UILabel!
+	@IBOutlet weak var para1: UITextField!
+	@IBOutlet weak var para2: UITextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		self.title = topic
+		
+		if let path = Bundle.main.path(forResource: "Details", ofType: "json") {
+			do {
+				let jsonData = try Data(contentsOf: URL(fileURLWithPath: path))
+				let jsonResult = try JSONSerialization.jsonObject(with: jsonData, options: .mutableLeaves)
+				if let result = jsonResult as? Dictionary<String, AnyObject>{
+					subtitle.text = result["Introduction"]?["SubTitle"] as? String
+				}
+			} catch {
+				
+			}
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
